07bc03ebb03c8744ff1a371b5725bcdf84e32967
samapriya <samapriya.roy@gmail.com>
2017-06-17 01:13:24 -0400

    Fixed Cookie Issue

--- geeadd/batch_uploader.py
+++ geeadd/batch_uploader.py
@@ -183,10 +183,10 @@ def __get_google_auth_session(username, password):
 
     auto = login_html.headers.get('X-Auto-Login')
     follow_up = unquote(unquote(auto)).split('continue=')[-1]
-    galx = login_html.cookies['GALX']
+    #galx = login_html.cookies['GALX']
 
     payload['continue'] = follow_up
-    payload['GALX'] = galx
+    #payload['GALX'] = galx
 
     session.post(authentication_url, data=payload)
 
