f43082f447635f92daaceb1da6e8a13ea32e8454
Samapriya Roy <samapriya.roy@gmail.com>
2018-03-08 08:50:11 -0700

    Updated to v0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='geeadd',
-    version='0.2.2',
+    version='0.2.3',
     packages=['geeadd'],
     package_data={'geeadd': ['logconfig.json']},
     url='https://github.com/samapriya/gee_asset_manager_addon',
